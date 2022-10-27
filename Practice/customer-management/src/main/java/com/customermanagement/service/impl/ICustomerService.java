dfe23caac0b2f5dc4dd05c4a602a305d230bd5bf
Compass <ndphong165@gmail.com>
2022-10-27 16:20:56 +0700

    first commit

---
+++ Practice/customer-management/src/main/java/com/customermanagement/service/impl/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.customermanagement.service.impl;
+
+import com.customermanagement.model.Customer;
+import com.customermanagement.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
